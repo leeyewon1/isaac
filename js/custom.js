0286676e7c37cc7bde83d51fe9f103f92b3c9d8d
leeyewon1 <148739266+leeyewon1@users.noreply.github.com>
2023-11-02 17:57:04 +0900

    Add files via upload

---
+++ js/custom.js
@@ -0,0 +1,88 @@
+$(function () {
+
+    $('.mainSlide').on('init afterChange', function (e, s, c) {
+        const current = $('.mainSlide .slick-current')
+
+        //console.log (c, s.slideCount);
+
+        $('.num span').text(s.slideCount);
+        $('.num strong').text(c ? (c + 1) : 1);
+
+        current
+            .addClass('on')
+            .siblings()
+            .removeClass('on');
+
+        $('.mainVisual .menu li')
+            .eq(0)
+            .addClass('on')
+
+        $('.mainVisual .menu li')
+            .eq(c)
+            .addClass('on')
+            .siblings()
+            .removeClass('on');
+
+    });
+
+
+    $('.mainSlide').slick({
+        autoplay: true,
+        arrows: false,
+        // dots: true,
+        pauseOnHover: false,
+    });
+
+
+
+    $('.mainVisual .arrows .left').on('click', function () {
+        // 슬라이드가 뒤로 가 : slick 홈페이지 참조.
+        $('.mainSlide').slick('slickPrev')
+    });
+    $('.mainVisual .arrows .right').on('click', function () {
+        // 슬라이드가 뒤로 가 : slick 홈페이지 참조.
+        $('.mainSlide').slick('slickNext')
+    });
+    $('.mainVisual .menu li').on('click', function (e) {
+        e.preventDefault();
+        //번호 가져오기 : 번호를 가져오는 것은 javascript에서 아주 중요한 일입니다.
+        const idx = $(this).index();
+        $('.num').text(idx + 1);
+        $('.mainSlide').slick('slickGoTo', idx);
+        $(this).addClass('on').siblings().removeClass('on');
+
+    });
+    $('.productSlide').slick({
+        slidesToShow: 3,
+        arrows: false,
+    });
+
+    $('.mainEvent .arrows .left').on('click', function () {
+        $('.productSlide').slick('slickPrev');
+    });
+
+    $('.mainEvent .arrows .right').on('click', function () {
+        $('.productSlide').slick('slickNext');
+    });
+
+    $('.totop').on('click', function () {
+        $('html , body').animate({ scrollTop: 0 }, 1000)
+    })
+
+    $(window).on('scroll', function () {
+        const sct = $(window).scrollTop();
+        if (sct > 200) {
+            $('.totop').addClass('on');
+        } else {
+            $('.totop').removeClass('on');
+        }
+    })
+
+
+
+
+
+
+});
+
+
